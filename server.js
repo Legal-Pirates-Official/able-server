88e9c616ab1ec230c7a40fba745ce85cae8f9a07
Lohith <srklohith05@gmail.com>
2022-04-12 12:08:35 +0530

    pm2

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const admin = require('./routes/admin');
 
 app.use('/admin', admin);
 
-app.listen(`${process.env.LISTENING_PORT}`, () => {
-	console.log(`Server is running on port ${process.env.LISTENING_PORT}`);
+app.listen(`${process.env.PORT}`, () => {
+	console.log(`Server is running on port ${process.env.PORT}`);
 });
