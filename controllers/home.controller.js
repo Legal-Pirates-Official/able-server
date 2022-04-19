727ab47d4207c6a7c9dbd1fe0621fc81027892cb
Lohith <srklohith05@gmail.com>
2022-04-19 11:20:51 +0530

    rv

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -11,72 +11,91 @@ exports.aboutget = async (req, res) => {
 };
 
 exports.aboutupdate = async (req, res) => {
-    await db.query('UPDATE homeabout SET description = ?', [req.body.values.description], (err, response) => { 
-        if (err) {
-            return console.log(err, 'error');
-        } else {
-            res.json(response);
-        }
-    });
-}
+	await db.query(
+		'UPDATE homeabout SET description = ?',
+		[req.body.values.description],
+		(err, response) => {
+			if (err) {
+				return console.log(err, 'error');
+			} else {
+				res.json(response);
+			}
+		}
+	);
+};
 
 exports.getyoutube = async (req, res) => {
-    await db.query('SELECT * FROM youtubevideos', (err, response) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200).json(response);
-        }
-    });
-}
+	await db.query('SELECT * FROM youtubevideos', (err, response) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.status(200).json(response);
+		}
+	});
+};
 
 exports.youtubeInsert = async (req, res) => {
-
-    await db.query('INSERT INTO youtubevideos (thumbnail, video_url) VALUES (?, ?)', [req.body.photo, req.body.values.video_url], (err, response) => {
-        if (err) {
-            return console.log(err, 'error');
-        } else {
-            res.json(response);
-        }
-    });
-}
+	await db.query(
+		'INSERT INTO youtubevideos (thumbnail, video_url) VALUES (?, ?)',
+		[req.body.photo, req.body.values.video_url],
+		(err, response) => {
+			if (err) {
+				return console.log(err, 'error');
+			} else {
+				res.json(response);
+			}
+		}
+	);
+};
 
 exports.deleteYoutube = async (req, res) => {
-    await db.query('DELETE FROM youtubevideos WHERE id = ?', [req.params.id], (err, response) => {
-        if (err) {
-            return console.log(err, 'error');
-        } else {
-            res.json(response);
-        }
-    });
-}
+	await db.query(
+		'DELETE FROM youtubevideos WHERE id = ?',
+		[req.params.id],
+		(err, response) => {
+			if (err) {
+				return console.log(err, 'error');
+			} else {
+				res.json(response);
+			}
+		}
+	);
+};
 
 exports.getEvents = async (req, res) => {
-    await db.query('SELECT * FROM liveevents', (err, response) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status(200).json(response);
-        }
-    });
-}
+	await db.query('SELECT * FROM liveevents', (err, response) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.status(200).json(response);
+		}
+	});
+};
 
 exports.eventsInsert = async (req, res) => {
-    await db.query('INSERT INTO liveevents (thumbnail, event_url) VALUES (?, ?)', [req.body.photo, req.body.values.event_url], (err, response) => {
-        if (err) {
-            return console.log(err, 'error');
-        } else {
-            res.json(response);
-        }
-    });
-}
+	await db.query(
+		'INSERT INTO liveevents (thumbnail, event_url) VALUES (?, ?)',
+		[req.body.photo, req.body.values.event_url],
+		(err, response) => {
+			if (err) {
+				return console.log(err, 'error');
+			} else {
+				res.json(response);
+			}
+		}
+	);
+};
 
 exports.deleteEvents = async (req, res) => {
-    await db.query('DELETE FROM liveevents WHERE id = ?', [req.params.id], (err, response) => {
-        if (err) {
-            return console.log(err, 'error');
-        } else {
-            res.json(response);
-        }
-    });
-}
+	await db.query(
+		'DELETE FROM liveevents WHERE id = ?',
+		[req.params.id],
+		(err, response) => {
+			if (err) {
+				return console.log(err, 'error');
+			} else {
+				res.json(response);
+			}
+		}
+	);
+};
